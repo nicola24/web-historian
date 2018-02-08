81029da8a141f2122a491447fc4213ce83941490
nicola24 <nicolas.peyrichou@gmail.com>
2018-02-07 20:07:47 -0800

    3 tests passed!

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
-
+const { URL } = require('url');
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
  * Consider using the `paths` object below to store frequently used file paths. This way,
@@ -12,7 +12,7 @@ var _ = require('underscore');
 exports.paths = {
   siteAssets: path.join(__dirname, '../web/public'),
   archivedSites: path.join(__dirname, '../archives/sites'),
-  list: path.join(__dirname, '../archives/sites.txt')
+  list: path.join(__dirname, '../archives/sites.txt'),
 };
 
 // Used for stubbing paths for tests, do not modify
@@ -26,12 +26,36 @@ exports.initialize = function(pathsObj) {
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback) {
+  fs.readFile(exports.paths.list, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      //convert to array 
+      var array = data.split('\n');
+      //give away formatted array to whatever needs it 
+      callback(err, array);
+    }
+  });
 };
 
 exports.isUrlInList = function(url, callback) {
+  fs.readFile(exports.paths.list, 'utf8', (err, data) => {
+    var array = data.split('\n');
+    var boolean = false;
+    if (array.includes(url)) {
+      boolean = true;
+    }
+    callback(err, boolean);
+  });   
 };
 
 exports.addUrlToList = function(url, callback) {
+  fs.writeFile(exports.paths.list, url, 'utf8', (err) => {
+    if (err) {
+      throw err;
+    }
+    callback(err);
+  });  
 };
 
 exports.isUrlArchived = function(url, callback) {
@@ -39,3 +63,31 @@ exports.isUrlArchived = function(url, callback) {
 
 exports.downloadUrls = function(urls) {
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
